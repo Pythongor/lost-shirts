659cd86bcb5585c911902a76322144ae5f3d3ede
Pythongor <djmozartd@gmail.com>
2020-12-25 22:44:57 +0200

    added sidebar

---
+++ scripts/sidebar-menu.js
@@ -0,0 +1,13 @@
+console.info('Hi');
+
+const sidebar = document.querySelector('.sidebar');
+const sidebarOpener = document.querySelector('.sidebar__hamburger');
+const sidebarCloser = document.querySelector('.sidebar__close');
+
+sidebarOpener.addEventListener('click', function() {
+    sidebar.classList.add('sidebar--opened');
+});
+
+sidebarCloser.addEventListener('click', function() {
+    sidebar.classList.remove('sidebar--opened');
+});
